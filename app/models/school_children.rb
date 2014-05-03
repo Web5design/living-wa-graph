bf8912631aa2049cb5284013c2e32328ef9b9aa1
Nikhil Sarwate <nikhil.sarwate@gmail.com>
2014-05-03 16:11:41 -0700

    Basic activeresouce for school_children

--- app/models/school_children.rb
+++ app/models/school_children.rb
@@ -1,3 +1,10 @@
-class SchoolChildren < ActiveRecord::Base
+class SchoolChildren < ActiveResource::Base
+  self.site = 'http://opendata.socrata.com/'
+  self.element_name = "resource"
+
+# 3ngx-mw5p
+  def self.get_all
+    get("3ngx-mw5p")
+  end
 
 end
